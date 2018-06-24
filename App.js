209c5edc76ece5cd87b82899e340a32e8b0bd5e7
Alexander <lex@work.com>
2018-06-24 18:32:14 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.viewText}>This is the start of the Beaconer project!!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewText: {
+    fontSize: 20
+  }
+});
